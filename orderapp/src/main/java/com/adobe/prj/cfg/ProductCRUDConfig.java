6069986938ff5ff6c4519c18ec38d6fa636bee5d
Banuprakash <banuprakashc@yahoo.co.in>
2022-11-07 17:35:11 +0530

    init

---
+++ orderapp/src/main/java/com/adobe/prj/cfg/ProductCRUDConfig.java
@@ -0,0 +1,48 @@
+package com.adobe.prj.cfg;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.adobe.prj.entity.Product;
+import com.adobe.prj.service.OrderService;
+
+@Configuration
+public class ProductCRUDConfig implements CommandLineRunner {
+	
+	@Autowired
+	private OrderService service;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		addProducts();
+		listProducts();
+	}
+
+	private void listProducts() {
+		List<Product> products = service.getProducts();
+		for(Product p : products) {
+			System.out.println(p); // toString()
+		}
+	}
+
+	private void addProducts() {
+		Product p1 = Product.builder()
+					.name("iPhone 14")
+					.price(89000.00)
+					.quantity(100)
+					.build();
+	
+		Product p2 = Product.builder()
+				.name("OnePlus12T")
+				.price(69000.00)
+				.quantity(100)
+				.build();
+		
+		service.insertProduct(p1);
+		service.insertProduct(p2);
+	}
+
+}
