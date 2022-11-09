3ecfab0fe246ea7808988e8ea2ca0a9b78222339
Banuprakash <banuprakashc@yahoo.co.in>
2022-11-09 15:01:33 +0530

    day 3

---
+++ orderapp/src/main/java/com/adobe/prj/api/GlobalControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.adobe.prj.api;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.adobe.prj.service.NotFoundException;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+	@ExceptionHandler(NotFoundException.class)
+	public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("message", ex.getMessage());
+		body.put("timestamp", new Date());
+		return new ResponseEntity<Object>(body, HttpStatus.NOT_FOUND);
+	}
+}
