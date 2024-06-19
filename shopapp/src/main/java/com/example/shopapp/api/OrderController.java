0db20897d00e30c8471c41fedf187150c8ee1e8e
Banuprakash <banuprakashc@yahoo.co.in>
2024-06-19 19:34:12 +0530

    restful

---
+++ shopapp/src/main/java/com/example/shopapp/api/OrderController.java
@@ -0,0 +1,14 @@
+package com.example.shopapp.api;
+
+import com.example.shopapp.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/products")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService service;
+    // Place Order and Get all orders
+}
